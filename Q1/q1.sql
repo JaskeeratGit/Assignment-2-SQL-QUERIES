d2f5ea8fc3173ae280499a96ac509640b67bbc02
Jaskeerat Singh <keerat00@gmail.com>
2023-10-12 17:31:21 -0600

    Q1 test 1

---
+++ Q1/q1.sql
@@ -0,0 +1,18 @@
+-- SQLite
+select  distinct usr 
+from users
+
+intersect 
+
+select distinct writer
+from tweets
+
+intersect 
+
+select distinct flwer
+from follows 
+
+intersect 
+
+select distinct owner 
+from lists;
