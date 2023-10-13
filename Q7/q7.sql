0c276ccef0e0ac3b1090efb03ed4cda54e0d8678
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-13 16:26:44 -0600

    Update q7.sql

--- Q7/q7.sql
+++ Q7/q7.sql
@@ -1,24 +1,36 @@
-WITH JohnDoeFollowers AS (
-    SELECT DISTINCT f.flwer
-    FROM users AS u
-    JOIN follows AS f ON u.usr = f.flwer
-    WHERE LOWER(u.name) = 'john doe'
+WITH JohnDoeFollowers AS 
+(
+    SELECT DISTINCT flwer
+    FROM follows, users
+    WHERE flwee = 
+    (
+        SELECT usr
+        FROM users
+        WHERE lower(name) = 'john doe'
+    )
 ),
-ListMembersCount AS (
-    SELECT li.lname, COUNT(*) AS member_count
+ListMembers AS (
+    SELECT i.lname, u.usr
+    FROM includes AS i
+    JOIN users AS u ON LOWER(i.member) = LOWER(u.usr)
+),
+ListMemberCounts AS 
+(
+    SELECT li.lname, COUNT(inc.member) AS member_count
     FROM lists AS li
-    JOIN includes AS inc ON li.lname = inc.lname
+    LEFT JOIN includes AS inc ON li.lname = inc.lname
     GROUP BY li.lname
 ),
-ListFollowersCount AS (
-    SELECT li.lname, COUNT(DISTINCT f.flwer) AS follower_count
-    FROM lists AS li
-    JOIN includes AS inc ON li.lname = inc.lname
-    JOIN follows AS f ON inc.member = f.flwee
-    GROUP BY li.lname
-)
+ListFollowerCounts AS
+(
+SELECT lm.lname, COUNT(DISTINCT CASE WHEN jdf.flwer IS NOT NULL THEN lm.usr END) AS follower_count
+    FROM ListMembers AS lm
+    LEFT JOIN JohnDoeFollowers AS jdf ON lm.usr = jdf.flwer
+    GROUP BY lm.lname
+    )
+
 SELECT lfc.lname
-FROM ListMembersCount AS lmc
-JOIN ListFollowersCount AS lfc ON lmc.lname = lfc.lname
+FROM ListMemberCounts AS lmc
+JOIN ListFollowerCounts AS lfc ON lmc.lname = lfc.lname
 WHERE lmc.member_count > 6
-  AND lfc.follower_count >= (0.5 * lmc.member_count)
+  AND lfc.follower_count >= 0.5 * lmc.member_count;
