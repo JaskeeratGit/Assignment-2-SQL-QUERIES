3aedfed99f8212f7a87589ac517ff7bf2da9bfc1
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-12 21:36:12 -0600

    Update q4.sql

--- Q4/q4.sql
+++ Q4/q4.sql
@@ -1,14 +1,24 @@
 SELECT users.usr, users.name
 FROM tweets, users, follows
 
-WHERE flwer =
+WHERE usr =
 (SELECT flwer 
 from follows
 where flwee = 
 (SELECT flwee
-from follows, users
-where users.name = 'John Doe')
+from follows
+where flwer = 
+(SELECT usr
+FROM users
+WHERE name = 'John Doe')
+)
 )
 
 GROUP BY usr
 HAVING COUNT(DISTINCT text) >= 2
+
+EXCEPT 
+
+SELECT usr, name
+FROM users
+WHERE users.name = 'John Doe'
