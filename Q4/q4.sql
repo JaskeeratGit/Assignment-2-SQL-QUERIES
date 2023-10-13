7d548ec637a7528c428a67d02af103ee3a13f5ca
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-13 12:23:36 -0600

    Update q4.sql

--- Q4/q4.sql
+++ Q4/q4.sql
@@ -1,18 +1,24 @@
 SELECT u.usr, u.name
 FROM users u
-WHERE u.usr != (SELECT usr FROM users WHERE LOWER(name) = 'john doe') -- Exclude John Doe
-AND u.usr IN (
-    SELECT DISTINCT t1.writer
-    FROM tweets t1
-    WHERE t1.writer <> (SELECT usr FROM users WHERE LOWER(name) = 'john doe') -- Exclude John Doe
-    AND t1.writer IN (
-        SELECT DISTINCT t2.writer
-        FROM tweets t2
-        WHERE t2.writer <> (SELECT usr FROM users WHERE LOWER(name) = 'john doe') -- Exclude John Doe
-    )
+WHERE u.usr IN (
+    SELECT writer
+    FROM tweets
+    GROUP BY writer
+    HAVING COUNT(DISTINCT text) >= 2
 )
-AND u.usr IN (
-    SELECT f.flwer
-    FROM follows f
-    WHERE f.flwee = (SELECT usr FROM users WHERE LOWER(name) = 'john doe') -- John Doe's followers
+
+INTERSECT
+
+SELECT u.usr, u.name
+FROM users u
+WHERE NOT EXISTS (
+    SELECT usr
+    FROM users j
+    WHERE LOWER(j.name) = 'john doe'
+    AND NOT EXISTS (
+        SELECT flwee
+        FROM follows f
+        WHERE f.flwer = u.usr
+        AND f.flwee = j.usr
+    )
 );
