d3935547b0df733787e157d44ff8594ce504d55d
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-13 11:59:03 -0600

    Update q2.sql

--- Q2/q2.sql
+++ Q2/q2.sql
@@ -1,9 +1,8 @@
-SELECT flwer,flwee
-FROM follows
-WHERE DATE('now','-90 day') >= start_date
-
-INTERSECT
-
-SELECT flwer, flwee
-FROM follows, users
-WHERE DATE('now','-90 day') < start_date AND users.name = 'John Doe'
+SELECT DISTINCT u1.usr
+FROM users AS u1
+JOIN follows AS f1 ON u1.usr = f1.flwer
+JOIN users AS u2 ON u2.usr = f1.flwee
+JOIN users AS johndoe ON LOWER(johndoe.name) = 'john doe'
+LEFT JOIN follows AS f2 ON johndoe.usr = f2.flwer AND f2.flwee = u2.usr
+WHERE f1.start_date <= date('now', '-90 days')
+  AND (f2.start_date > date('now', '-90 days'))
