f1496d7244b3dd8dbfd0eac5652453cf252c3d82
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-12 21:40:44 -0600

    Update q4.sql

--- Q4/q4.sql
+++ Q4/q4.sql
@@ -1,19 +1,17 @@
-SELECT users.usr, users.name
-FROM tweets, users, follows
 
-WHERE usr =
-(SELECT flwer 
-from follows
-where flwee = 
-(SELECT flwee
-from follows
-where flwer = 
-(SELECT usr
-FROM users
-WHERE name = 'John Doe')
-)
-)
-
-GROUP BY usr
-HAVING COUNT(DISTINCT text) >= 2
-
+SELECT u.usr, u.name
+FROM users AS u
+WHERE (
+    SELECT COUNT(DISTINCT t.tdate)
+    FROM tweets AS t
+    WHERE t.writer = u.usr
+) >= 2
+AND NOT EXISTS (
+    SELECT f.flwee
+    FROM follows AS f
+    WHERE LOWER(f.flwer) = 'john doe'
+    EXCEPT
+    SELECT f1.flwee
+    FROM follows AS f1
+    WHERE f1.flwer = u.usr
+);
