dd75d6eabbf0fbb2632dc8f4a7b093a1dac10b1c
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-12 18:02:50 -0600

    Update q1.sql

--- Q1/q1.sql
+++ Q1/q1.sql
@@ -3,12 +3,10 @@ FROM tweets
 
 INTERSECT
 
--- Users with followers
 SELECT DISTINCT flwee
 FROM follows
 
 INTERSECT
 
--- Users who are members of at least one list
 SELECT DISTINCT member
 FROM includes;
