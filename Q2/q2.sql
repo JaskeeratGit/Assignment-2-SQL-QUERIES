e3dd2a80e959940b0071976e4948f58ecbeb86b3
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-12 19:57:09 -0600

    Update q2.sql

--- Q2/q2.sql
+++ Q2/q2.sql
@@ -1,12 +1,11 @@
-SELECT flwee
+SELECT  flwer FROM follows WHERE flwee = 
+(SELECT flwee
 FROM users, follows
 WHERE users.name = 'John Doe'
 and DATE('now', '-90 day') < start_date
-
 
 INTERSECT
 
 SELECT flwer
 FROM follows
-WHERE DATE('now', '-90 day') >= start_date;
-
+WHERE DATE('now', '-90 day') >= start_date);
