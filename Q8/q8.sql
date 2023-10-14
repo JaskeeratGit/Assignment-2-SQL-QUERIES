09f8ac402d3657eedf353c9c57bfcaa405443c90
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-13 19:27:53 -0600

    Update q8.sql

--- Q8/q8.sql
+++ Q8/q8.sql
@@ -1,27 +1,37 @@
-WITH MonthlyActivity AS (
+WITH MonthlyTweetCounts AS (
     SELECT
-        strftime('%m', month) AS month,
-        SUM(tcnt) AS tcnt,
-        SUM(rep_cnt) AS rep_cnt,
-        SUM(ret_cnt) AS ret_cnt
+        strftime('%m', tdate) AS month,
+        SUM(CASE WHEN replyto_w IS NULL THEN 1 ELSE 0 END) AS tcnt,
+        SUM(CASE WHEN replyto_w IS NOT NULL THEN 1 ELSE 0 END) AS rep_cnt
     FROM
-        (SELECT strftime('%m', tdate) AS month, COUNT(*) AS tcnt, 0 AS rep_cnt, 0 AS ret_cnt
-         FROM tweets
-         WHERE strftime('%Y', tdate) = '2023'
-         GROUP BY month
-         UNION ALL
-         SELECT strftime('%m', rdate) AS month, 0 AS tcnt, 0 AS rep_cnt, COUNT(*) AS ret_cnt
-         FROM retweets
-         WHERE strftime('%Y', rdate) = '2023'
-         GROUP BY month)
-    GROUP BY month
+        tweets
+    WHERE
+        strftime('%Y', tdate) = '2023'
+    GROUP BY
+        month
+),
+MonthlyRetweetCounts AS (
+    SELECT
+        strftime('%m', rdate) AS month,
+        COUNT(*) AS ret_cnt
+    FROM
+        retweets
+    WHERE
+        strftime('%Y', rdate) = '2023'
+    GROUP BY
+        month
 )
 SELECT
-    month,
-    tcnt,
-    rep_cnt,
-    ret_cnt,
-    (tcnt + rep_cnt + ret_cnt) AS total
-FROM MonthlyActivity
-WHERE (tcnt > 0 OR rep_cnt > 0)
-ORDER BY month;
+    COALESCE(MTC.month, MRC.month) AS month,
+    COALESCE(tcnt, 0) AS tcnt,
+    COALESCE(rep_cnt, 0) AS rep_cnt,
+    COALESCE(ret_cnt, 0) AS ret_cnt,
+    COALESCE(tcnt, 0) + COALESCE(rep_cnt, 0) + COALESCE(ret_cnt, 0) AS total
+FROM
+    MonthlyTweetCounts MTC
+FULL OUTER JOIN
+    MonthlyRetweetCounts MRC ON MTC.month = MRC.month
+WHERE
+    (tcnt > 0 OR rep_cnt > 0)
+ORDER BY
+    month;
