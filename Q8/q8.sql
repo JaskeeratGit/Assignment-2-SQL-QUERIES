2ab5faa85f9f6ef2997131d0dea6099e6f708d5d
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-13 18:42:12 -0600

    Update q8.sql

--- Q8/q8.sql
+++ Q8/q8.sql
@@ -1,8 +1,8 @@
 WITH MonthlyTweetCounts AS (
     SELECT
-        strftime('%m', tdate) AS month,
-        COUNT(CASE WHEN replyto_w IS NULL THEN 1 ELSE NULL END) AS tcnt,
-        COUNT(CASE WHEN replyto_w IS NOT NULL THEN 1 ELSE NULL END) AS rep_cnt
+        strftime('%Y-%m', tdate) AS month,
+        SUM(CASE WHEN replyto_w IS NULL THEN 1 ELSE 0 END) AS tcnt,
+        SUM(CASE WHEN replyto_w IS NOT NULL THEN 1 ELSE 0 END) AS rep_cnt
     FROM
         tweets
     WHERE
@@ -12,28 +12,26 @@ WITH MonthlyTweetCounts AS (
 ),
 MonthlyRetweetCounts AS (
     SELECT
-        strftime('%m', tdate) AS month,
+        strftime('%Y-%m', rdate) AS month,
         COUNT(*) AS ret_cnt
     FROM
         retweets
     WHERE
-        strftime('%Y', tdate) = '2023'
+        strftime('%Y', rdate) = '2023'
     GROUP BY
         month
 )
 SELECT
-    MT.month AS month,
+    COALESCE(MTC.month, MRC.month) AS month,
     COALESCE(tcnt, 0) AS tcnt,
     COALESCE(rep_cnt, 0) AS rep_cnt,
     COALESCE(ret_cnt, 0) AS ret_cnt,
     COALESCE(tcnt, 0) + COALESCE(rep_cnt, 0) + COALESCE(ret_cnt, 0) AS total
 FROM
-    (SELECT DISTINCT month FROM MonthlyTweetCounts
-    UNION
-    SELECT DISTINCT month FROM MonthlyRetweetCounts) AS MT
-LEFT JOIN
-    MonthlyTweetCounts MTC ON MT.month = MTC.month
-LEFT JOIN
-    MonthlyRetweetCounts MRC ON MT.month = MRC.month
+    MonthlyTweetCounts MTC
+FULL OUTER JOIN
+    MonthlyRetweetCounts MRC ON MTC.month = MRC.month
+WHERE
+    (tcnt > 0 OR rep_cnt > 0)
 ORDER BY
     month;
