d875a886b222ef9995b09c8da78d543efd6c2367
JaskeeratGit <138101993+JaskeeratGit@users.noreply.github.com>
2023-10-13 19:58:49 -0600

    Update q9.sql

--- Q9/q9.sql
+++ Q9/q9.sql
@@ -1,41 +1,45 @@
-CREATE VIEW tstat
-AS
-  SELECT T.writer,
-         T.tdate,
-         T.text,
-         COALESCE(C.rep_cnt, 0) AS rep_cnt,
-         COALESCE(R.ret_cnt, 0) AS ret_cnt,
-         COALESCE(M.sim_cnt, 0) AS sim_cnt
-  FROM   tweets T
-         LEFT JOIN (SELECT writer,
-                           tdate,
-                           Count(*) AS rep_cnt
-                    FROM   tweets
-                    WHERE  replyto_w IS NOT NULL
-                           AND replyto_d IS NOT NULL
-                    GROUP  BY writer,
-                              tdate) C
-                ON T.writer = C.writer
-                   AND T.tdate = C.tdate
-         LEFT JOIN (SELECT writer,
-                           tdate,
-                           Count(*) AS ret_cnt
-                    FROM   retweets
-                    GROUP  BY writer,
-                              tdate) R
-                ON T.writer = R.writer
-                   AND T.tdate = R.tdate
-         LEFT JOIN (SELECT T.writer,
-                           T.tdate,
-                           Count(*) AS sim_cnt
-                    FROM   tweets T
-                           JOIN mentions M
-                             ON T.writer = M.writer
-                                AND T.tdate = M.tdate
-                    GROUP  BY T.writer,
-                              T.tdate) M
-                ON T.writer = M.writer
-                   AND T.tdate = M.tdate;
-
-SELECT *
-FROM   tstat; 
+CREATE VIEW tStat AS
+SELECT
+    T.writer,
+    T.tdate,
+    T.text,
+    COALESCE(C.rep_cnt, 0) AS rep_cnt,
+    COALESCE(R.ret_cnt, 0) AS ret_cnt,
+    COALESCE(M.sim_cnt, 0) AS sim_cnt
+FROM tweets T
+LEFT JOIN (
+    SELECT
+        writer,
+        tdate,
+        COUNT(*) AS rep_cnt
+    FROM tweets
+    WHERE replyto_w IS NOT NULL AND replyto_d IS NOT NULL
+    GROUP BY writer, tdate
+) C ON T.writer = C.writer AND T.tdate = C.tdate
+LEFT JOIN (
+    SELECT
+        writer,
+        tdate,
+        COUNT(*) AS ret_cnt
+    FROM retweets
+    GROUP BY writer, tdate
+) R ON T.writer = R.writer AND T.tdate = R.tdate
+LEFT JOIN (
+    SELECT
+        T.writer,
+        T.tdate,
+        COUNT(*) AS sim_cnt
+    FROM tweets T
+    WHERE EXISTS (
+        SELECT 1
+        FROM mentions M
+        WHERE T.writer = M.writer
+          AND T.tdate = M.tdate
+          AND M.term IN (
+              SELECT DISTINCT term
+              FROM hashtags
+          )
+    )
+    GROUP BY T.writer, T.tdate
+) M ON T.writer = M.writer AND T.tdate = M.tdate;
+SELECT * from tstat;
